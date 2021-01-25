d85f29c0ae75b80d858ac2c0ce40e61e1c51ef26
layen67 <lkbcontact@gmail.com>
2021-01-25 01:16:30 +0100

    Update scalway-installok.sh

--- scalway-installok.sh
+++ scalway-installok.sh
@@ -287,7 +287,7 @@ service nginx restart;
 #
 apt-get update;
 apt-get install -y docker.io;
-curl -L https://github.com/docker/compose/releases/download/1.28.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+curl -L --fail https://github.com/docker/compose/releases/download/1.28.0/run.sh -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
 
 
