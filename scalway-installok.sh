2111702a3f5ec4923871a55eb8940bbda68aff4f
layen67 <lkbcontact@gmail.com>
2021-02-08 17:39:02 +0100

    Update scalway-installok.sh

--- scalway-installok.sh
+++ scalway-installok.sh
@@ -173,7 +173,7 @@ apt-add-repository ppa:brightbox/ruby-ng -y;
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8;
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main';
 curl -sL https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -;
-add-apt-repository 'deb http://www.rabbitmq.com/debian/ testing main';
+add-apt-repository 'deb https://dl.bintray.com/rabbitmq/debian xenial main';
 apt update;
 export DEBIAN_FRONTEND=noninteractive;
 apt install -y libnetcdf-dev libssl-dev libcrypto++-dev libgmp-dev ruby-mysql2 ruby2.3 ruby2.3-dev build-essential mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano;
