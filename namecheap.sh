1cf3e987d25e7270f0b74b7ee3f3d70c76da33d5
layen67 <lkbcontact@gmail.com>
2021-02-11 00:15:22 +0100

    Update namecheap.sh

--- namecheap.sh
+++ namecheap.sh
@@ -74,13 +74,13 @@ dpkg -i erlang-solutions_1.0_all.deb;
 sudo apt-get update -y;
 apt-get install erlang;
 ## Install prerequisites
-sudo apt-get install curl gnupg -y
+sudo apt-get install curl gnupg -y;
 
 ## Install RabbitMQ signing key
-curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | sudo apt-key add -
+curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | sudo apt-key add -;
 
 ## Install apt HTTPS transport
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https;
 
 ## Add Bintray repositories that provision latest RabbitMQ and Erlang 23.x releases
 sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
@@ -88,16 +88,16 @@ sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
 ## Change component to "erlang-22.x" to install the latest 22.x version.
 ## "bionic" as distribution name should work for any later Ubuntu or Debian release.
 ## See the release to distribution mapping table in RabbitMQ doc guides to learn more.
-deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang
+deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang;
 ## Installs latest RabbitMQ release
-deb https://dl.bintray.com/rabbitmq/debian bionic main
+deb https://dl.bintray.com/rabbitmq/debian bionic main;
 EOF
 
 ## Update package indices
-sudo apt-get update -y
+sudo apt-get update -y;
 
 ## Install rabbitmq-server and its dependencies
-sudo apt-get install rabbitmq-server -y --fix-missing
+sudo apt-get install rabbitmq-server -y --fix-missing;
 
 
 
